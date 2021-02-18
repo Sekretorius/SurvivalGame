38c842d169f3a2bd31f0b70fa27cb0191b7b558d
Sekretorius <54936405+Sekretorius@users.noreply.github.com>
2021-02-18 22:40:41 +0200

    Side Scroller camera bounds
    Camera doesn't go out of the map

--- SurvivalGeim/Assets/Scripts/CameraFollow.cs
+++ SurvivalGeim/Assets/Scripts/CameraFollow.cs
@@ -7,7 +7,15 @@ public class CameraFollow : MonoBehaviour
     public static CameraFollow instance;
 
     [SerializeField]
+    private CompositeCollider2D collider;
+
+    [SerializeField]
     private GameObject target;
+
+    private Camera camera;
+    private float halfExtent;
+    
+
 
     void Awake()
     {
@@ -17,11 +25,24 @@ public class CameraFollow : MonoBehaviour
         }
         else
             Destroy(this);
+
+        if (collider == null)
+            collider = GameObject.FindGameObjectWithTag("Ground").GetComponent<CompositeCollider2D>();
+
+    }
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        halfExtent = camera.orthographicSize * Screen.width / Screen.height;
+
     }
 
     private void LateUpdate()
     {
-        gameObject.transform.position = new Vector3(target.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        transform.position = new Vector3(
+            Mathf.Clamp(target.transform.position.x, collider.bounds.min.x + halfExtent, collider.bounds.max.x - halfExtent),
+            gameObject.transform.position.y, gameObject.transform.position.z);
     }
 
     public void RefreshPosition()
