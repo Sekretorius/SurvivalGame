c19f536260292c2578e0117137f6013e83f98d44
Sekretorius <kaspa1221@gmail.com>
2021-05-10 05:58:25 +0300

    Basic Forest intro animation

---
+++ SurvivalGeim/Assets/ScriptedIntro.cs
@@ -0,0 +1,44 @@
+using InventorySystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ScriptedIntro : MonoBehaviour
+{
+    public PlayableDirector director;
+
+    public CanvasGroup canvas;
+
+    public bool start = true;
+
+    void Start()
+    {
+        if (start)
+            Init();
+    }
+
+    public void Init()
+    {
+        CameraFollow.instance.RefreshPosition();
+        if (InventoryManager.Instance)
+            InventoryManager.Instance.gameObject.SetActive(false);
+
+        CameraFollow.instance.block = true;
+        PlayerController.instance.block = true;
+
+        director.Play();
+
+        canvas.alpha = 0;
+    }
+
+    public void IntroEnded()
+    {
+        CameraFollow.instance.block = false;
+        PlayerController.instance.block = false;
+        if (InventoryManager.Instance)
+            InventoryManager.Instance.gameObject.SetActive(true);
+
+        canvas.alpha = 1;
+    }
+}
